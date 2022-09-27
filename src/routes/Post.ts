a89876ab33ac9377b0df6c64bb149795c2f62fed
Pedro Fontes <pedro7fontes@gmail.com>
2022-09-27 00:35:13 -0300

    refactor: post route

--- src/routes/Post.ts
+++ src/routes/Post.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import controller from "../controllers/PostController";
 import { authMiddleware } from "../middlewares/auth";
+import { postMiddleware } from "../middlewares/post";
 import { checkRole } from "../middlewares/checkRole";
 
 const router = express.Router();
 
 // Não autenticada
 
-router.post("/post/new", authMiddleware, controller.createPost);
+router.post("/post", authMiddleware, postMiddleware, controller.createPost);
 router.get("/post/:postId", authMiddleware, controller.readPost);
 router.get("/feed/", authMiddleware, controller.readAllPosts);
 
